8cac554c8dd03d3b8e25ebd02ed0181f3aedc640
Will Granger <twgranger@gmail.com>
2019-08-20 15:11:47 -0500

    Merge Themes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-import Home from './Home';
-import Projects from './screens/ProjectsIndex';
-import { Grommet } from 'grommet';
-import grommetTheme from './theme';
+import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import merge from 'lodash/merge'
+import { Grommet } from 'grommet'
+import zooTheme from '@zooniverse/grommet-theme'
+import './App.css'
+import Home from './Home'
+import Projects from './screens/ProjectsIndex'
+import baseTheme from './theme'
 
 function App() {
+  const mergedTheme = merge({}, baseTheme, zooTheme)
   return (
     <Router>
       <>
         <main>
-          <Grommet theme={grommetTheme}>
+          <Grommet theme={mergedTheme}>
             <Route path="/" exact component={Home} />
             <Route path="/projects" component={Projects} />
           </Grommet>
