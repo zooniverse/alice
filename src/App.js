5ddc439d6a964afff39d0edbaa4ea4dfea55a750
Will Granger <twgranger@gmail.com>
2019-08-28 10:22:21 -0500

    Use BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import merge from 'lodash/merge'
 import { Grommet } from 'grommet'
 import zooTheme from '@zooniverse/grommet-theme'
