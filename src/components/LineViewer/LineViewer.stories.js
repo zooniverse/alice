a930819bcc32db2848bcdc537e3bb7cc0d042976
Will Granger <twgranger@gmail.com>
2019-09-18 13:29:16 -0500

    Create LineViewer Story File

---
+++ src/components/LineViewer/LineViewer.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Box, Grommet } from 'grommet';
+import LineViewerContainer from './LineViewerContainer'
+import zooTheme from '@zooniverse/grommet-theme'
+
+storiesOf('LineViewer', module)
+  .add('Default', () => (
+    <Grommet theme={zooTheme}>
+      <Box background='#D8D8D8' height='xlarge'>
+        <LineViewerContainer />
+      </Box>
+    </Grommet>
+  ))
