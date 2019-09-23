91bdd892acd955a5bdc470433848a70532e17b43
Will Granger <twgranger@gmail.com>
2019-09-23 14:45:53 -0500

    Add panoptes-client

--- src/store/AuthStore.js
+++ src/store/AuthStore.js
@@ -1,10 +1,11 @@
 import { types } from 'mobx-state-tree'
+import { auth } from 'panoptes-client'
 
 const AuthStore = types.model('AuthStore', {
   user: types.optional(types.frozen({}), null),
 }).actions(self => ({
   login () {
-    console.log("hello world");
+    console.log(auth);
   },
 }))
 
