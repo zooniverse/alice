b752dadd7b449660bdb617747ee2ff7642312b00
Will Granger <twgranger@gmail.com>
2019-09-26 14:22:14 -0500

    Stub out checkCurrent call on app component

--- src/components/Badge/Badge.spec.js
+++ src/components/Badge/Badge.spec.js
@@ -2,8 +2,14 @@ import { shallow } from 'enzyme'
 import React from 'react'
 import Badge from './Badge'
 
+let wrapper
+
 describe('Component > Badge', function () {
+  beforeAll(function () {
+    wrapper = shallow(<Badge />);
+  })
+
   it('should render without crashing', function () {
-    shallow(<Badge />);
+    expect(wrapper).toBeDefined()
   })
 })
--- src/components/Badge/BadgeContainer.js
+++ src/components/Badge/BadgeContainer.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { observer } from 'mobx-react'
 import AppContext from 'store'
 import Badge from './Badge'
 
 function BadgeContainer () {
-  const store = React.useContext(AppContext)
+  const store = useContext(AppContext)
   const user = store.auth.user
 
   const name = user && user.display_name
