091268ab5eda602a417bc2287e8cb0bc1e772aaa
Will Granger <twgranger@gmail.com>
2019-10-02 13:16:05 -0500

    Add min and max scale

--- src/store/ImageStore.js
+++ src/store/ImageStore.js
@@ -1,7 +1,9 @@
 import { types } from 'mobx-state-tree'
 
 const ROTATION_STEP = 90;
-const STEP = 0.2;
+const STEP = 0.4;
+const MAX_STEP = 2;
+const MIN_STEP = 0.6;
 
 const ImageStore = types.model('ImageStore', {
   rotation: types.optional(types.number, 0),
@@ -10,8 +12,14 @@ const ImageStore = types.model('ImageStore', {
   rotate() {
     self.rotation = self.rotation === 360 ? 0 : self.rotation + ROTATION_STEP
   },
-  zoomIn() { self.scale += STEP },
-  zoomOut() { self.scale -= STEP }
+  zoomIn() {
+    if (self.scale < MAX_STEP)
+      self.scale += STEP
+  },
+  zoomOut() {
+    if (self.scale > MIN_STEP)
+      self.scale -= STEP
+  }
 }))
 
 export { ImageStore }
