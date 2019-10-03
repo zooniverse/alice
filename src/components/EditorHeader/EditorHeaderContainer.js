710b9b2bc9edc6e17fc97c295b6201cedfc528b2
Will Granger <twgranger@gmail.com>
2019-10-03 15:02:05 -0500

    Render Buttons by Path

--- src/components/EditorHeader/EditorHeaderContainer.js
+++ src/components/EditorHeader/EditorHeaderContainer.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import EditorHeader from './EditorHeader'
+import SearchButton from './components/HeaderButton/SearchButtonContainer'
+import DownloadSetData from './components/HeaderButton/DownloadSetDataContainer'
+import MarkApproved from './components/MarkApproved'
+import UndoButton from './components/HeaderButton/UndoButtonContainer'
+import SaveButton from './components/HeaderButton/SaveButtonContainer'
+import LayoutButton from './components/HeaderButton/LayoutButtonContainer'
+import MoreButton from './components/HeaderButton/MoreButtonContainer'
+import { SUBJECTS_PATH, EDIT_PATH } from 'paths'
 
-class EditorHeaderContainer extends Component {
-  render () {
-    return <EditorHeader />
+function getHeaderTools(path) {
+  switch (path) {
+    case SUBJECTS_PATH:
+      return [DownloadSetData, SearchButton]
+    case EDIT_PATH:
+      return [MarkApproved, UndoButton, SaveButton, LayoutButton, MoreButton]
+    default:
+      return []
   }
 }
 
-export default EditorHeaderContainer
+export default function EditorHeaderContainer() {
+  const buttons = getHeaderTools(window.location.pathname)
+  return <EditorHeader buttons={buttons} />
+}
---
+++ src/paths.js
@@ -0,0 +1,13 @@
+const PROJECTS_PATH = '/projects'
+const WORKFLOWS_PATH = '/projects/workflows'
+const SUBJECT_SETS_PATH = '/projects/workflows/subject-sets'
+const SUBJECTS_PATH = '/projects/workflows/subject-sets/subjects'
+const EDIT_PATH = '/projects/workflows/subject-sets/subjects/edit'
+
+export {
+  PROJECTS_PATH,
+  WORKFLOWS_PATH,
+  SUBJECT_SETS_PATH,
+  SUBJECTS_PATH,
+  EDIT_PATH
+}
