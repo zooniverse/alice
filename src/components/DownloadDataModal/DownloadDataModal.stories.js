21ae5508ddb557143eeb43bf3f000b6a352b4e68
Will Granger <twgranger@gmail.com>
2019-10-07 16:12:26 -0500

    Finish Styling DownloadSubjectData Modal

---
+++ src/components/DownloadDataModal/DownloadDataModal.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Box, Grommet } from 'grommet';
+import DownloadDataModal from './DownloadDataModal'
+import zooTheme from '@zooniverse/grommet-theme'
+
+storiesOf('DownloadDataModal', module)
+  .add('Default', () => (
+    <Grommet theme={zooTheme}>
+      <Box background='#D8D8D8' height='xlarge'>
+        <DownloadDataModal />
+      </Box>
+    </Grommet>
+  ))
