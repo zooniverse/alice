645a7c7197aa5c0cd1d7d91f7b3fbdcf290d6347
Will Granger <twgranger@gmail.com>
2019-10-08 10:50:36 -0500

    Allow Reducer Toggle

--- src/components/AggregationSettings/AggregationSettingsContainer.js
+++ src/components/AggregationSettings/AggregationSettingsContainer.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import AggregationSettings from './AggregationSettings'
 
-export default function AggregationSettingsContainer() {
-  return <AggregationSettings />
+const REDUCERS = {
+  OPTICS: 'OPTICS',
+  DBSCAN: 'DBSCAN'
+}
+
+function AggregationSettingsContainer() {
+  const [ selectedReducer, selectReducer ] = React.useState();
+  return (
+    <AggregationSettings
+      selectReducer={selectReducer}
+      selectedReducer={selectedReducer}
+    />
+  )
+}
+
+export {
+  AggregationSettingsContainer,
+  REDUCERS
 }
--- src/components/AggregationSettings/ChooseReducer.js
+++ src/components/AggregationSettings/ChooseReducer.js
@@ -1,29 +1,64 @@
 import React from 'react'
-import { Box, Text } from 'grommet'
+import { Box, Button, Text } from 'grommet'
+import { func, string } from 'prop-types'
+import { REDUCERS } from './AggregationSettingsContainer'
+import styled from 'styled-components'
 
-export default function ChooseReducer() {
+const OpticsBox = styled(Box)`
+  background: ${props => props.selectedReducer === REDUCERS.OPTICS && '#F5F5F5'};
+  border: ${props => props.selectedReducer === REDUCERS.OPTICS ?
+    '1px solid #979797' : '1px solid transparent'};
+`
+
+const DbScanBox = styled(Box)`
+  background: ${props => props.selectedReducer === REDUCERS.DBSCAN && '#F5F5F5'};
+  border: ${props => props.selectedReducer === REDUCERS.DBSCAN ?
+    '1px solid #979797' : '1px solid transparent'};
+`
+
+export default function ChooseReducer({ selectReducer, selectedReducer }) {
   return (
-    <Box>
+    <Box gap='xsmall'>
       <Text>
         Choose a clustering algorithm based on the way in which transcriptions
         have been made.
       </Text>
       <Box
         direction='row'
-        gap='medium'
-        pad='small'
       >
-        <Box gap='xsmall'>
-          <Box background='light-4' height='8em' fill='horizontal'/>
-          <Text>OPTICS Reducer</Text>
-          <Text>Short description of when to use this algorithm</Text>
-        </Box>
-        <Box gap='xsmall'>
-          <Box background='light-4' height='8em' fill='horizontal'/>
-          <Text>DBSCAN Reducer</Text>
-          <Text>Short description of when to use this algorithm</Text>
-        </Box>
+        <Button
+          label={
+            <OpticsBox selectedReducer={selectedReducer} gap='xsmall' pad='small'>
+              <Box background='light-4' height='8em' fill='horizontal'/>
+              <Text>OPTICS Reducer</Text>
+              <Text>Short description of when to use this algorithm</Text>
+            </OpticsBox>
+          }
+          onClick={() => { selectReducer(REDUCERS.OPTICS)}}
+          plain
+        />
+        <Button
+          label={
+            <DbScanBox selectedReducer={selectedReducer} gap='xsmall' pad='small'>
+              <Box background='light-4' height='8em' fill='horizontal'/>
+              <Text>DBSCAN Reducer</Text>
+              <Text>Short description of when to use this algorithm</Text>
+            </DbScanBox>
+          }
+          onClick={() => { selectReducer(REDUCERS.DBSCAN)}}
+          plain
+        />
       </Box>
     </Box>
   )
+}
+
+ChooseReducer.defaultProps = {
+  selectReducer: () => {},
+  selectedReducer: null
+}
+
+ChooseReducer.propTypes = {
+  selectReducer: func,
+  selectedReducer: string
 }
