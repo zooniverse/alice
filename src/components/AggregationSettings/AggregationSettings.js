55177878a255d78cfcaf4e3a03228d5dbc4a450a
Will Granger <twgranger@gmail.com>
2019-10-08 14:13:59 -0500

    Initial Working Aggregation Form

--- src/components/AggregationSettings/AggregationSettings.js
+++ src/components/AggregationSettings/AggregationSettings.js
@@ -4,8 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import { func, string } from 'prop-types'
 import ChooseReducer from './ChooseReducer'
+import AdjustReducer from './AdjustReducer'
+import { SCREENS } from './AggregationSettingsContainer'
 
-export default function AggregationSettings({ selectReducer, selectedReducer }) {
+export default function AggregationSettings({ currentScreen, selectReducer, selectedReducer, setScreen }) {
+  const disableNext = selectedReducer === null;
+
   return (
     <Box
       background='white'
@@ -22,32 +26,47 @@ export default function AggregationSettings({ selectReducer, selectedReducer }) 
         <Text>Edit Aggregation Settings</Text>
         <FontAwesomeIcon icon={faTimesCircle} />
       </Box>
-      <Box height='medium' pad='small'>
-        <ChooseReducer
-          selectReducer={selectReducer}
-          selectedReducer={selectedReducer}
-        />
+      <Box pad='small'>
+        {(currentScreen !== SCREENS.CHOOSE_REDUCER) ? (
+          <ChooseReducer
+            selectReducer={selectReducer}
+            selectedReducer={selectedReducer}
+          />
+        ) : <AdjustReducer /> }
       </Box>
       <Box
         border='top'
         direction='row'
-        gap='small'
-        justify='end'
+        justify='between'
         pad='small'
       >
-        <Button label={<Text size='small'>CLOSE AND CANCEL</Text>} plain/>
-        <Button label={<Text size='small'>NEXT</Text>} plain/>
+        {(currentScreen !== SCREENS.ADJUST_REDUCER) && (
+          <Button label={<Text>BACK TO ALGORITHM CHOICE</Text>} plain />
+        )}
+        <Box align='end' direction='row' gap='small'>
+          <Button label={<Text size='small'>CLOSE AND CANCEL</Text>} plain/>
+          <Button
+            disabled={disableNext}
+            label={<Text size='small'>NEXT</Text>}
+            onClick={() => { setScreen(SCREENS.ADJUST_REDUCER) }}
+            plain
+          />
+        </Box>
       </Box>
     </Box>
   )
 }
 
 ChooseReducer.defaultProps = {
+  currentScreen: null,
   selectReducer: () => {},
-  selectedReducer: null
+  selectedReducer: null,
+  setScreen: () => {}
 }
 
 ChooseReducer.propTypes = {
+  currentScreen: string,
   selectReducer: func,
-  selectedReducer: string
+  selectedReducer: string,
+  setScreen: func
 }
--- src/components/AggregationSettings/AggregationSettingsContainer.js
+++ src/components/AggregationSettings/AggregationSettingsContainer.js
@@ -6,17 +6,27 @@ const REDUCERS = {
   DBSCAN: 'DBSCAN'
 }
 
+const SCREENS = {
+  CHOOSE_REDUCER: 'CHOOSE_REDUCER',
+  ADJUST_REDUCER: 'ADJUST_REDUCER'
+}
+
 function AggregationSettingsContainer() {
-  const [ selectedReducer, selectReducer ] = React.useState();
+  const [ selectedReducer, selectReducer ] = React.useState(null);
+  const [ currentScreen, setScreen ] = React.useState(SCREENS.CHOOSE_REDUCER);
+
   return (
     <AggregationSettings
+      currentScreen={currentScreen}
       selectReducer={selectReducer}
       selectedReducer={selectedReducer}
+      setScreen={setScreen}
     />
   )
 }
 
 export {
   AggregationSettingsContainer,
-  REDUCERS
+  REDUCERS,
+  SCREENS
 }
