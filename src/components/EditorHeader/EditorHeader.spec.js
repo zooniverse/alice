611355b54c2e1aefcdb74807437762e913098b40
Will Granger <twgranger@gmail.com>
2019-10-16 15:20:18 -0500

    Update EditorHeader Tests

--- src/components/EditorHeader/EditorHeader.spec.js
+++ src/components/EditorHeader/EditorHeader.spec.js
@@ -1,5 +1,6 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import MetadataButton from './components/MetadataButton'
 import EditorHeader from './EditorHeader'
 import MarkApproved from './components/MarkApproved'
 
@@ -14,5 +15,10 @@ describe('Component > EditorHeader', function () {
   it('should render buttons with received props', function () {
     wrapper = shallow(<EditorHeader buttons={[MarkApproved]}/>)
     expect(wrapper.find(MarkApproved).length).toBe(1)
+  })
+
+  it('should render the metadata button when available', function () {
+    wrapper = shallow(<EditorHeader showMetadata />)
+    expect(wrapper.find(MetadataButton).length).toBe(1)
   })
 })
