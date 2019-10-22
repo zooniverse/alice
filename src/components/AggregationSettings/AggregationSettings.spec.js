ae45cf2e852612a750b058be596f8c5c1abee6a9
Will Granger <twgranger@gmail.com>
2019-10-22 11:48:11 -0500

    Update AggregationSettings tests and panoptes-client package

--- src/components/AggregationSettings/AggregationSettings.spec.js
+++ src/components/AggregationSettings/AggregationSettings.spec.js
@@ -1,15 +1,43 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import AggregationSettings from './AggregationSettings'
+import { REDUCERS } from './AggregationSettingsContainer'
+import DBScanReducer from './DBScanReducer'
+import OpticsReducer from './OpticsReducer'
+import ChooseReducer from './ChooseReducer'
+import Confirmation from './Confirmation'
 
 let wrapper
 
 describe('Component > AggregationSettings', function () {
   beforeEach(function() {
-    wrapper = shallow(<AggregationSettings />);
+    wrapper = shallow(<AggregationSettings currentScreen={REDUCERS.CHOOSE} />);
   })
 
   it('should render without crashing', function () {
     expect(wrapper).toBeDefined()
+  })
+
+  it('should show the ChooseReducer form', function () {
+    expect(wrapper.find(ChooseReducer).length).toBe(1)
+  })
+
+  describe('props > currentScreen', function () {
+    it('should show the OpticsReducer form', function () {
+      wrapper = shallow(<AggregationSettings currentScreen={REDUCERS.OPTICS} />)
+      expect(wrapper.find(OpticsReducer).length).toBe(1)
+    })
+
+    it('should show the DBScanReducer form', function () {
+      wrapper = shallow(<AggregationSettings currentScreen={REDUCERS.DBSCAN} />)
+      expect(wrapper.find(DBScanReducer).length).toBe(1)
+    })
+  })
+
+  describe('props > confirmationCallback', function () {
+    it('should show the Confirmation overlay with valid function', function () {
+      wrapper = shallow(<AggregationSettings confirmationCallback={() => {}} />)
+      expect(wrapper.find(Confirmation).length).toBe(1)
+    })
   })
 })
