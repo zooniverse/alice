ada0eef6328a574a678e3743ea27417eded43aba
Will Granger <twgranger@gmail.com>
2019-10-24 15:10:54 -0500

    Add SubjectLockedModal Tests

--- src/components/Modals/SubjectLockedModal/SubjectLockedModal.spec.js
+++ src/components/Modals/SubjectLockedModal/SubjectLockedModal.spec.js
@@ -1,15 +1,23 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import { Button } from 'grommet'
 import SubjectLockedModal from './SubjectLockedModal'
 
 let wrapper
+let onBackSpy = jest.fn()
 
 describe('Component > SubjectLockedModal', function () {
   beforeAll(function () {
-    wrapper = shallow(<SubjectLockedModal />);
+    wrapper = shallow(<SubjectLockedModal onBack={onBackSpy} />);
   })
 
   it('should render without crashing', function () {
     expect(wrapper).toBeDefined()
+  })
+
+  it('should call the onBack function with button press', function () {
+    const backButton = wrapper.find(Button).first()
+    backButton.simulate('click')
+    expect(onBackSpy).toHaveBeenCalled()
   })
 })
