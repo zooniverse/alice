8b0491c2b8bbdc4bc924621802614645fc909ccd
Will Granger <twgranger@gmail.com>
2019-10-24 13:12:14 -0500

    Update MarkApproved Container Tests

--- src/components/EditorHeader/components/MarkApproved/MarkApprovedContainer.spec.js
+++ src/components/EditorHeader/components/MarkApproved/MarkApprovedContainer.spec.js
@@ -3,13 +3,32 @@ import React from 'react'
 import MarkApprovedContainer from './MarkApprovedContainer'
 
 let wrapper
+let useStateSpy
+const setState = jest.fn()
+const toggleModalSpy = jest.fn()
+const contextValues = {
+  modal: {
+    toggleModal: toggleModalSpy
+  }
+}
 
 describe('Component > MarkApprovedContainer', function () {
   beforeEach(function() {
+    jest
+      .spyOn(React, 'useContext')
+      .mockImplementation(() => contextValues )
+    useStateSpy = jest.spyOn(React, 'useState')
+    useStateSpy.mockImplementation((init) => [false, setState])
     wrapper = shallow(<MarkApprovedContainer />);
   })
 
   it('should render without crashing', function () {
     expect(wrapper).toBeDefined()
+  })
+
+  it('should call the toggleModal method with child onClick', function() {
+    wrapper.props().onChange()
+    expect(toggleModalSpy).toHaveBeenCalledWith('unapproved')
+    expect(setState).toHaveBeenCalled()
   })
 })
