9bc32df6d7afdb4c1da8384fb32915261b9660b0
Will Granger <twgranger@gmail.com>
2019-10-28 09:51:37 -0500

    Only call fetchSubject from url params

--- src/screens/SubjectsIndex/SubjectsPageContainer.js
+++ src/screens/SubjectsIndex/SubjectsPageContainer.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Box } from 'grommet'
-import AppContext from 'store'
 import { generatePath, withRouter } from 'react-router-dom'
 import { EDIT_PATH } from 'paths'
 import ResourcesTable from '../../components/ResourcesTable'
 import { mockColumns, mockData } from './mock'
 
 function SubjectsPageContainer (props) {
-  const store = React.useContext(AppContext)
   const onSelection = (id) => {
-    store.subject.fetchSubject(id, true)
     const nextPath = generatePath(EDIT_PATH, { subject: id, ...props.match.params})
     props.history.replace(nextPath)
   }
