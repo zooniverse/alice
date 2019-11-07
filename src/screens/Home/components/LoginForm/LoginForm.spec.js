8e48035beedb44b34ec7838eb3e30b2f4e941d4e
Will Granger <twgranger@gmail.com>
2019-11-07 15:21:49 -0600

    Write StepNavigation Tests

--- src/screens/Home/components/LoginForm/LoginForm.spec.js
+++ src/screens/Home/components/LoginForm/LoginForm.spec.js
@@ -1,9 +1,15 @@
 import { shallow } from 'enzyme'
 import React from 'react'
-
+import { Formik } from 'formik'
+import { Button } from 'grommet'
 import LoginForm from './LoginForm'
 
 let wrapper
+let validateFormSpy = jest.fn().mockResolvedValue(true)
+const initialValues = {
+  login: '',
+  password: '',
+}
 
 describe('Component > LoginForm', function () {
   beforeEach(function () {
@@ -12,5 +18,23 @@ describe('Component > LoginForm', function () {
 
   it('should render without crashing', function () {
     expect(wrapper).toBeDefined()
+  })
+
+  describe('Form children', function () {
+    let form
+    beforeEach(function() {
+      const Form = wrapper.find(Formik).first().props().children
+      form = shallow(
+        <Form
+          values={initialValues}
+          validateForm={validateFormSpy}
+        />)
+    })
+
+    it('should call validate form with submit', function () {
+      const button = form.find(Button).first()
+      button.props().onClick()
+      expect(validateFormSpy).toHaveBeenCalled()
+    })
   })
 })
