4ba25a1a5d18f8cc0a4e690bbb566fdac282bd90
Will Granger <twgranger@gmail.com>
2019-11-15 14:42:16 -0600

    Create AnnotationsPane Tests

---
+++ src/components/SubjectViewer/components/AnnotationsPane/AnnotationsPane.spec.js
@@ -0,0 +1,24 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import AnnotationsPane from './AnnotationsPane'
+
+let wrapper
+const lines = [
+  [{ x: 0, y: 0 }, { x: 10, y: 0 }],
+  [{ x: 0, y: 0 }, { x: 0, y: 10 }]
+]
+
+describe('Component > AnnotationsPane', function () {
+  beforeEach(function() {
+    wrapper = shallow(<AnnotationsPane lines={lines} />);
+  })
+
+  it('should render without crashing', function () {
+    expect(wrapper).toBeDefined()
+  })
+
+  it('should render the correct lines', function () {
+    const circles = wrapper.find('circle')
+    expect(circles.length).toBe(4)
+  })
+})
