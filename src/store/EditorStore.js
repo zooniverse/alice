0e8211811aac0649a2ee07425a6598628b97f686
Will Granger <twgranger@gmail.com>
2019-11-18 15:27:03 -0600

    Remove Unused updateViewerSize function

--- src/store/EditorStore.js
+++ src/store/EditorStore.js
@@ -10,10 +10,6 @@ const EditorStore = types.model('EditorStore', {
 }).actions(self => ({
   toggleLayout() {
     self.layout = (self.layout === LAYOUT.ROW) ? LAYOUT.COLUMN : LAYOUT.ROW
-  },
-
-  updateViewerSize(width, height) {
-    console.log(width, height);
   }
 }))
 
