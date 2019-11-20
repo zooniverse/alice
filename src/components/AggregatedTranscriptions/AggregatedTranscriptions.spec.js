ecadb0eab1f89ac4fd81a752d2d3308768ea48ab
Will Granger <twgranger@gmail.com>
2019-11-20 14:21:59 -0600

    Update AggregatedTranscriptions Tests

--- src/components/AggregatedTranscriptions/AggregatedTranscriptions.spec.js
+++ src/components/AggregatedTranscriptions/AggregatedTranscriptions.spec.js
@@ -1,9 +1,15 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import Overlay from '../Overlay'
 import AggregatedTranscriptions from './AggregatedTranscriptions'
 
 describe('Component > AggregatedTranscriptions', function () {
   it('should render without crashing', function () {
     shallow(<AggregatedTranscriptions />);
+  })
+
+  it('should display an overlay when given prop', function () {
+    const wrapper = shallow(<AggregatedTranscriptions showOverlay />)
+    expect(wrapper.find(Overlay).length).toBe(1)
   })
 })
