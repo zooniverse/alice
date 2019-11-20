35e079d3a44c2778898efa835827f641122e5fc8
Will Granger <twgranger@gmail.com>
2019-11-20 13:11:35 -0600

    Remove Unused ZoomInButtonContainer

--- src/components/ZoomInButtonContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import AppContext from 'store'
-import ZoomInButton from './ZoomInButton'
-
-export default function ZoomInButtonContainer() {
-  const store = React.useContext(AppContext)
-  return <ZoomInButton onClick={store.image.zoomIn} />
-}
