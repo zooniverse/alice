b786ac67c26823d99b79456811fe8d91181055fe
Will Granger <twgranger@gmail.com>
2019-12-10 12:14:03 -0600

    Update App Tests to Check For Initialization

--- src/App.test.js
+++ src/App.test.js
@@ -9,12 +9,17 @@ const contextValues = {
 }
 
 describe('App', function () {
-  beforeAll(function () {
+  it('should render without crashing', function () {
     jest.spyOn(React, 'useContext').mockImplementation((context) => contextValues)
     wrapper = shallow(<App />);
+    expect(wrapper).toBeDefined()
   })
 
-  it('should render without crashing', function () {
-    expect(wrapper).toBeDefined()
+  it('should render nothing when not initialized', function () {
+    const revisedContext = Object.assign({}, contextValues)
+    revisedContext.initialized = false
+    jest.spyOn(React, 'useContext').mockImplementation((context) => revisedContext)
+    wrapper = shallow(<App />);
+    expect(wrapper.getElement()).toBe(null)
   })
 })
