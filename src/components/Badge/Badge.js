9237a75b87d4b3e4b5a64efd734c96c819fccf29
Will Granger <twgranger@gmail.com>
2019-12-11 14:36:53 -0600

    Adjust Badge Drop Button

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Box, Menu, Text } from 'grommet'
+import { Box, DropButton, Text } from 'grommet'
+import { FormDown, FormUp } from 'grommet-icons'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import HeaderButton from '../EditorHeader/components/HeaderButton'
 import DefaultAvatar from '../../images/simple-avatar.png'
 
 const StyledAvatar = styled.img`
   border-radius: 100%;
   height: 2.5em;
   width: auto;
-  margin: 0 0.75em;
+  margin-left: 0.75em;
   object-fit: cover;
 `
 
@@ -17,18 +19,30 @@ const CapitalText = styled(Text)`
   text-transform: uppercase;
 `
 
-function Badge ({ disabled, name, role, signOut, src }) {
+function Badge ({ disabled, isOpen, name, role, setOpen, signOut, src }) {
+  const Icon = isOpen ? FormDown : FormUp
+
   return (
-    <Box align='center' direction='row'>
+    <Box align='center' direction='row' gap='xsmall'>
       <StyledAvatar alt={`${name} Avatar`} fallback={DefaultAvatar} src={src} />
       <Box>
         <Text>{name}</Text>
         <CapitalText color='dark-5' size='xsmall'>{role}</CapitalText>
       </Box>
-      <Menu
+      <DropButton
+        alignSelf='center'
         disabled={disabled}
         dropAlign={{ right: 'right', top: 'bottom' }}
-        items={[{ label: 'Log Out', onClick: signOut }]}
+        label={<Box><Icon/></Box>}
+        dropContent={
+          <Box background='white' width='5em'>
+            <HeaderButton label='Log Out' onClick={signOut} />
+          </Box>
+        }
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={isOpen}
+        plain
       />
     </Box>
   )
@@ -36,16 +50,20 @@ function Badge ({ disabled, name, role, signOut, src }) {
 
 Badge.defaultProps = {
   disabled: false,
+  isOpen: false,
   name: '',
   role: '',
+  setOpen: () => {},
   signOut: () => {},
   src: DefaultAvatar
 }
 
 Badge.propTypes = {
   disabled: PropTypes.bool,
+  isOpen: PropTypes.bool,
   name: PropTypes.string,
   role: PropTypes.string,
+  setOpen: () => {},
   signOut: PropTypes.func,
   src: PropTypes.string
 }
--- src/components/Badge/BadgeContainer.js
+++ src/components/Badge/BadgeContainer.js
@@ -5,13 +5,23 @@ import Badge from './Badge'
 
 function BadgeContainer ({ disabled }) {
   const store = React.useContext(AppContext)
+  const [isOpen, setOpen] = React.useState(false)
   const user = store.auth.user
 
   const name = user && user.display_name
   const signOut = store.auth.logout
   const src = user && user.avatar_src
 
-  return <Badge disabled={disabled} name={name} signOut={signOut} src={src} />
+  return (
+    <Badge
+      disabled={disabled}
+      isOpen={isOpen}
+      name={name}
+      setOpen={setOpen}
+      signOut={signOut}
+      src={src}
+    />
+  )
 }
 
 export default observer(BadgeContainer)
