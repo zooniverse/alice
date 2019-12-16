e5f9df847647193ab080d26bf7dc2d5435c7ba1c
Will Granger <twgranger@gmail.com>
2019-12-16 14:25:38 -0600

    Check LineViewer appears when active transcription

--- src/components/AggregatedTranscriptions/AggregatedTranscriptions.spec.js
+++ src/components/AggregatedTranscriptions/AggregatedTranscriptions.spec.js
@@ -2,6 +2,7 @@ import { shallow } from 'enzyme'
 import React from 'react'
 import Overlay from '../Overlay'
 import AggregatedTranscriptions from './AggregatedTranscriptions'
+import LineViewer from '../LineViewer'
 
 describe('Component > AggregatedTranscriptions', function () {
   it('should render without crashing', function () {
@@ -11,5 +12,10 @@ describe('Component > AggregatedTranscriptions', function () {
   it('should display an overlay when given prop', function () {
     const wrapper = shallow(<AggregatedTranscriptions showOverlay />)
     expect(wrapper.find(Overlay).length).toBe(1)
+  })
+
+  it('should display a LineViewer when given prop', function () {
+    const wrapper = shallow(<AggregatedTranscriptions showTranscription />)
+    expect(wrapper.find(LineViewer).length).toBe(1)
   })
 })
