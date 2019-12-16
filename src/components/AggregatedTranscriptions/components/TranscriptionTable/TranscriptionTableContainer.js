4df7ee209a6b172701c478250ba6fd32f05900bf
Will Granger <twgranger@gmail.com>
2019-12-16 14:39:27 -0600

    Test for toggleTranscription in TranscriptionTableContainer

--- src/components/AggregatedTranscriptions/components/TranscriptionTable/TranscriptionTableContainer.js
+++ src/components/AggregatedTranscriptions/components/TranscriptionTable/TranscriptionTableContainer.js
@@ -6,14 +6,11 @@ import data from './mockData'
 
 function TranscriptionTableContainer() {
   const store = React.useContext(AppContext)
-  const toggleTranscription = () => {
-    store.aggregations.toggleTranscription()
-  }
 
   return (
     <TranscriptionTable
       data={data}
-      toggleTranscription={toggleTranscription}
+      toggleTranscription={store.aggregations.toggleTranscription}
     />
   )
 }
--- src/components/AggregatedTranscriptions/components/TranscriptionTable/TranscriptionTableContainer.spec.js
+++ src/components/AggregatedTranscriptions/components/TranscriptionTable/TranscriptionTableContainer.spec.js
@@ -3,13 +3,27 @@ import React from 'react'
 import TranscriptionTableContainer from './TranscriptionTableContainer'
 
 let wrapper
+const toggleTranscriptionSpy = jest.fn()
+const contextValues = {
+  aggregations: {
+    toggleTranscription: toggleTranscriptionSpy
+  }
+}
 
 describe('Component > TranscriptionTableContainer', function () {
   beforeEach(function() {
+    jest
+      .spyOn(React, 'useContext')
+      .mockImplementation(() => contextValues )
     wrapper = shallow(<TranscriptionTableContainer />);
   })
 
   it('should render without crashing', function () {
     expect(wrapper).toBeDefined()
+  })
+
+  it('should call the toggleTranscription function', function () {
+    wrapper.props().toggleTranscription()
+    expect(toggleTranscriptionSpy).toHaveBeenCalled()
   })
 })
