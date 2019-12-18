4b18eb1c71024a80c71a32560ce393972492e5c9
Will Granger <twgranger@gmail.com>
2019-12-18 14:35:18 -0600

    Test clearTag prop in SearchTag

--- src/components/ResourcesTable/components/SearchTags/SearchTag.spec.js
+++ src/components/ResourcesTable/components/SearchTags/SearchTag.spec.js
@@ -1,15 +1,23 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import { Button } from 'grommet'
 import SearchTag from './SearchTag'
 
 let wrapper
+const clearTagSpy = jest.fn()
 
 describe('Component > SearchTag', function () {
   beforeEach(function() {
-    wrapper = shallow(<SearchTag />);
+    wrapper = shallow(<SearchTag clearTag={clearTagSpy} />);
   })
 
   it('should render without crashing', function () {
     expect(wrapper).toBeDefined()
+  })
+
+  it('should trigger the clearTag prop', function () {
+    const closeBtn = wrapper.find(Button).first()
+    closeBtn.props().onClick()
+    expect(clearTagSpy).toHaveBeenCalled()
   })
 })
