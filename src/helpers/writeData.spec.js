a1dfe51c5f57307f8d2515a5138aa76be736ef62
Will Granger <twgranger@gmail.com>
2020-01-24 13:11:24 -0600

    Write Tests for writeData Helper Method

---
+++ src/helpers/writeData.spec.js
@@ -0,0 +1,13 @@
+import writeDate from './writeDate'
+
+describe('Helper > writeDate', function () {
+  it('should return the correct date format', function () {
+    const result = writeDate('Tue Jan 16 2020 19:00:00 GMT-0500')
+    expect(result).toBe('January 16, 2020')
+  })
+
+  it('should return empty string when given nothing', function () {
+    const result = writeDate(null)
+    expect(result).toBe('')
+  })
+})
