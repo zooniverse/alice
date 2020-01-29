4a5fb1565b91fe625ffea502a62eb5e1bf596666
Will Granger <twgranger@gmail.com>
2020-01-29 10:38:57 -0600

    Adjust Line Design

--- src/components/SubjectViewer/components/AnnotationsPane/AnnotationsPane.js
+++ src/components/SubjectViewer/components/AnnotationsPane/AnnotationsPane.js
@@ -2,32 +2,47 @@ import React from 'react'
 import { array, string } from 'prop-types'
 import indexToRainbow from 'helpers/indexToRainbow'
 
-function AnnotationsPane({ lines, x, y }) {
+const CIRCLE_WIDTH = 10
+
+function AnnotationsPane({ lines, isExtract, x, y }) {
   const offset = `translate(${x}, ${y})`
+  const dashArray = isExtract ? '5' : '0'
 
   return (
     <g transform={offset}>
       {lines.map((line, index) => {
-        const color = indexToRainbow(index % 11) 
+        const color = isExtract ? 'white' : indexToRainbow(index % 11)
         const svgPoints = []
         const svgLines = []
 
         for (let i = 0; i < line.length; i++) {
           const point = line[i]
+          const fill = i === 0 ? color : 'transparent'
           svgPoints.push(
             <circle
               key={`SVG_DOT_${i}`}
-              cx={point.x} cy={point.y} r={10} fill={color}
+              cx={point.x}
+              cy={point.y}
+              r={CIRCLE_WIDTH}
+              fill={fill}
+              stroke={color}
+              strokeWidth='2'
             />
           )
           if (i > 0) {
             const prevPoint = line[i -1]
+            const x1 = prevPoint.x
+            let x2 = point.x
+            const isLeftToRight = x1 < x2
+            isLeftToRight ? x2 -= CIRCLE_WIDTH : x2 += CIRCLE_WIDTH
+
             svgLines.push(
               <line
                 key={`SVG_LINE_${i}`}
-                x1={prevPoint.x} y1={prevPoint.y}
-                x2={point.x} y2={point.y}
+                x1={x1} y1={prevPoint.y}
+                x2={x2} y2={point.y}
                 stroke={color} strokeWidth="2"
+                strokeDasharray={dashArray}
               />
             )
           }
--- src/components/SubjectViewer/components/AnnotationsPane/ExtractsPaneContainer.js
+++ src/components/SubjectViewer/components/AnnotationsPane/ExtractsPaneContainer.js
@@ -12,13 +12,14 @@ function ExtractsPaneContainer({ x, y }) {
   const extracts = store.transcriptions.extracts
   const validExtracts = extracts.filter(extract => extract.data[`frame${index}`])
   parsedLines = validExtracts.map(transcription => constructCoordinatesFromExtract(transcription.data[`frame${index}`]))
-  console.log('extracts', validExtracts);
+  console.log('extracts', parsedLines);
   console.log('reductions', store.transcriptions.current.text.frame0);
 
   return parsedLines.map((line, i) => {
     return (
       <AnnotationsPane
         key={`EXTRACT_LINE_${i}`}
+        isExtract
         x={x}
         y={y}
         lines={line}
