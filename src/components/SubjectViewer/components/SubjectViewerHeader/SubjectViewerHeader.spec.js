44e547fa1ac9315be416f39d1244a3451a899551
Will Granger <twgranger@gmail.com>
2020-01-31 09:02:38 -0600

    Update SubjectViewerHeader Tests

--- src/components/SubjectViewer/components/SubjectViewerHeader/SubjectViewerHeader.spec.js
+++ src/components/SubjectViewer/components/SubjectViewerHeader/SubjectViewerHeader.spec.js
@@ -1,9 +1,26 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import { Button } from 'grommet'
 import SubjectViewerHeader from './SubjectViewerHeader'
 
 describe('Component > SubjectViewerHeader', function () {
   it('should render without crashing', function () {
     shallow(<SubjectViewerHeader />);
+  })
+
+  describe('button with lines visible', function () {
+    it("should display 'hide all lines'", function () {
+      const wrapper = shallow(<SubjectViewerHeader />);
+      const buttonLabelText = wrapper.find(Button).first().props().label.props.children
+      expect(buttonLabelText).toBe('HIDE ALL LINES')
+    })
+  })
+
+  describe('button with lines hidden', function () {
+    it("should display 'hide all lines'", function () {
+      const wrapper = shallow(<SubjectViewerHeader linesVisible={false} />);
+      const buttonLabelText = wrapper.find(Button).first().props().label.props.children
+      expect(buttonLabelText).toBe('VIEW ALL LINES')
+    })
   })
 })
