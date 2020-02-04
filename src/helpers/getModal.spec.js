8d0f8297708851fd1628697a61afa7a6c9861242
Will Granger <twgranger@gmail.com>
2020-02-04 15:16:08 -0600

    Add getModal Tests

---
+++ src/helpers/getModal.spec.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import getModal from './getModal'
+import MODALS from './modals'
+
+describe('Helper > getModal', function () {
+  it('should return a valid component', function () {
+    Object.values(MODALS).forEach(modal => {
+      const Component = getModal(modal)
+      const rendered = shallow(<Component />)
+      expect(rendered).toBeDefined()
+    })
+  })
+
+  it('should return null without a valid component', function () {
+    expect(getModal('unknown')).toBe(null)
+  })
+})
