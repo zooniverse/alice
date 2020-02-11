865f7e753257fb329542ba3ee238fa0287121915
Will Granger <twgranger@gmail.com>
2020-02-11 09:47:02 -0600

    Write DeleteModal Tests

---
+++ src/components/LineViewer/components/DeleteModal/DeleteModal.spec.js
@@ -0,0 +1,37 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import { Button } from 'grommet'
+import DeleteModal from './DeleteModal'
+
+let wrapper
+const deleteLineSpy = jest.fn()
+const toggleModalSpy = jest.fn()
+
+describe('Component > DeleteModal', function () {
+  beforeEach(function () {
+    wrapper = shallow(
+      <DeleteModal
+        deleteLine={deleteLineSpy}
+        toggleModal={toggleModalSpy}
+      />)
+  })
+
+  afterEach(() => jest.clearAllMocks());
+
+  it('should render without crashing', function () {
+    expect(wrapper).toBeDefined()
+  })
+
+  it('should toggle the modal on cancel', function () {
+    const cancelBtn = wrapper.find(Button).first()
+    cancelBtn.simulate('click')
+    expect(toggleModalSpy).toHaveBeenCalled()
+  })
+
+  it('should delete the ine on line deletion', function () {
+    const deleteBtn = wrapper.find(Button).last()
+    deleteBtn.simulate('click')
+    expect(toggleModalSpy).toHaveBeenCalled()
+    expect(deleteLineSpy).toHaveBeenCalled()
+  })
+})
