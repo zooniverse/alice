dda011e18e00badd214aaf13ddd08e9877682d73
Will Granger <twgranger@gmail.com>
2020-02-12 13:13:43 -0600

    Disable Undo Button

--- src/components/EditorHeader/components/HeaderButton/UndoButtonContainer.js
+++ src/components/EditorHeader/components/HeaderButton/UndoButtonContainer.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import { Refresh } from 'grommet-icons'
 import { undoManager } from 'store/AppStore'
+import { observer } from 'mobx-react'
 import HeaderButton from './HeaderButton'
 
 function UndoButtonContainer({ disabled }) {
-  const onClick = () => {
-    console.log(undoManager.canUndo);
-    undoManager.canUndo && undoManager.undo()
-  }
+  const onUndo = () => undoManager.canUndo && undoManager.undo()
+  const disableUndo = disabled || !undoManager.canUndo
 
   return (
     <HeaderButton
-      disabled={disabled}
+      disabled={disableUndo}
       icon={<Refresh color='#555555' size='small'/>}
-      label={'Undo'}
-      onClick={onClick}
+      label='Undo'
+      onClick={onUndo}
     />
   )
 }
 
-export default UndoButtonContainer
+export default observer(UndoButtonContainer)
