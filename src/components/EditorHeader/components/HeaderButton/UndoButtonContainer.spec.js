e7c075ae58659c42edb7d1d64e07075d36c6ee6c
Will Granger <twgranger@gmail.com>
2020-02-13 15:19:39 -0600

    Update UndoButtonContainer Tests

--- src/components/EditorHeader/components/HeaderButton/UndoButtonContainer.spec.js
+++ src/components/EditorHeader/components/HeaderButton/UndoButtonContainer.spec.js
@@ -1,8 +1,10 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import * as mod from 'store/AppStore'
 import UndoButtonContainer from './UndoButtonContainer'
 
 let wrapper
+const undoSpy = jest.fn()
 
 describe('Component > UndoButton', function () {
   beforeEach(function() {
@@ -11,5 +13,14 @@ describe('Component > UndoButton', function () {
 
   it('should render without crashing', function () {
     expect(wrapper).toBeDefined()
+  })
+
+  it('should call on undo', function () {
+    mod.undoManager = {
+      canUndo: true,
+      undo: undoSpy
+    }
+    wrapper.props().onClick()
+    expect(undoSpy).toHaveBeenCalled()
   })
 })
