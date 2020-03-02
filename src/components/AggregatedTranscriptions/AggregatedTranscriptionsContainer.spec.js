6eedb153bec737ade99945a07da3902ef9aea48c
Will Granger <twgranger@gmail.com>
2020-03-02 12:31:42 -0600

    Update AggregatedTranscriptionsContainer Tests

--- src/components/AggregatedTranscriptions/AggregatedTranscriptionsContainer.spec.js
+++ src/components/AggregatedTranscriptions/AggregatedTranscriptionsContainer.spec.js
@@ -2,14 +2,30 @@ import { shallow } from 'enzyme'
 import React from 'react'
 import AggregatedTranscriptionsContainer from './AggregatedTranscriptionsContainer'
 
+const addLineSpy = jest.fn()
+const context = {
+  aggregations: {},
+  transcriptions: {
+    activeTranscriptionIndex: 0,
+    addLine: addLineSpy
+  }
+}
 let wrapper
 
 describe('Component > AggregatedTranscriptionsContainer', function () {
   beforeEach(function() {
+    jest
+      .spyOn(React, 'useContext')
+      .mockImplementation(() => context)
     wrapper = shallow(<AggregatedTranscriptionsContainer />);
   })
 
   it('should render without crashing', function () {
     expect(wrapper).toBeDefined()
+  })
+
+  it('should add a line', function () {
+    wrapper.props().addLine()
+    expect(addLineSpy).toHaveBeenCalled()
   })
 })
