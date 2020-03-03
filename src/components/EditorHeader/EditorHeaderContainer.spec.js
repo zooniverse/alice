e6e100890aea23bd06940dfb0009131957f1406e
Will Granger <twgranger@gmail.com>
2020-03-03 11:45:58 -0600

    Update EditorHeaderContainer Tests

--- src/components/EditorHeader/EditorHeaderContainer.spec.js
+++ src/components/EditorHeader/EditorHeaderContainer.spec.js
@@ -25,6 +25,16 @@ const contextValues = {
     approved: false
   }
 }
+const EDIT_PATH_HISTORY = {
+  location: {
+    pathname: EDIT_PATH
+  }
+}
+const SUBJECTS_PATH_HISTORY = {
+  location: {
+    pathname: SUBJECTS_PATH
+  }
+}
 
 describe('Component > EditorHeaderContainer', function () {
   beforeEach(function() {
@@ -40,24 +50,14 @@ describe('Component > EditorHeaderContainer', function () {
   })
 
   it('should render the correct buttons on the subjects index', function () {
-    const history = {
-      location: {
-        pathname: SUBJECTS_PATH
-      }
-    }
-    wrapper = shallow(<EditorHeaderContainer history={history} />);
+    wrapper = shallow(<EditorHeaderContainer history={SUBJECTS_PATH_HISTORY} />);
     const wrapperButtons = wrapper.find('EditorHeader').props().buttons
     expect(wrapperButtons).toEqual(expect.arrayContaining([DownloadSetData]))
     expect(wrapperButtons).toEqual(expect.arrayContaining([SearchButton]))
   })
 
   it('should render the correct buttons on the edit page', function () {
-    const history = {
-      location: {
-        pathname: EDIT_PATH
-      }
-    }
-    wrapper = shallow(<EditorHeaderContainer history={history} />);
+    wrapper = shallow(<EditorHeaderContainer history={EDIT_PATH_HISTORY} />);
     const wrapperButtons = wrapper.find('EditorHeader').props().buttons
     expect(wrapperButtons).toEqual(expect.arrayContaining([MarkApproved]))
     expect(wrapperButtons).toEqual(expect.arrayContaining([UndoButton]))
@@ -75,5 +75,29 @@ describe('Component > EditorHeaderContainer', function () {
     wrapper = shallow(<EditorHeaderContainer history={history} />);
     const wrapperButtons = wrapper.find('EditorHeader').props().buttons
     expect(wrapperButtons.length).toBe(0)
+  })
+
+  describe('when a viewer', function () {
+    beforeEach(function() {
+      const viewerContext = Object.assign(contextValues, {})
+      viewerContext.projects.isViewer = true
+      jest
+        .spyOn(React, 'useContext')
+        .mockImplementation(() => viewerContext )
+    })
+
+    it('should display the correct edit tools', function () {
+      wrapper = shallow(<EditorHeaderContainer history={EDIT_PATH_HISTORY} />);
+      const wrapperButtons = wrapper.find('EditorHeader').props().buttons
+      expect(wrapperButtons.length).toBe(1)
+      expect(wrapperButtons).toEqual(expect.arrayContaining([LayoutButton]))
+    })
+
+    it('should display the correct subject tools', function () {
+      wrapper = shallow(<EditorHeaderContainer history={SUBJECTS_PATH_HISTORY} />);
+      const wrapperButtons = wrapper.find('EditorHeader').props().buttons
+      expect(wrapperButtons.length).toBe(1)
+      expect(wrapperButtons).toEqual(expect.arrayContaining([SearchButton]))
+    })
   })
 })
