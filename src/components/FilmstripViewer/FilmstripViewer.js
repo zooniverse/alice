bac2e92e46e9e99b2d4de6ab1916069d8433dff0
Will Granger <twgranger@gmail.com>
2020-03-05 12:49:29 -0600

    Adjust Filmstrip Title and Border

--- src/components/FilmstripViewer/FilmstripViewer.js
+++ src/components/FilmstripViewer/FilmstripViewer.js
@@ -22,7 +22,7 @@ function FilmstripViewer ({ disabled, selectImage, subjectIndex, images, isOpen,
     <RelativeBox background='#FFFFFF' pad='xsmall' round={{ size: 'xsmall', corner: 'top' }}>
       {disabled && <Overlay />}
       <Box direction='row' justify='between'>
-        <Text>All pages</Text>
+        <Text>All Pages</Text>
         {!isOpen && (
           <StepNavigation
             activeStep={subjectIndex}
