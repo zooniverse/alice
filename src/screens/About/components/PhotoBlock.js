5517c2e601600f07a478d89375278d9d0594cf83
Will Granger <twgranger@gmail.com>
2020-04-13 15:17:03 -0500

    Finish Setup Section

--- src/screens/About/components/PhotoBlock.js
+++ src/screens/About/components/PhotoBlock.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import { Box, Image, Text } from 'grommet'
 import { arrayOf, shape, string } from 'prop-types'
+import { Markdown } from 'markdownz'
 import styled from 'styled-components'
 
 const CapitalText = styled(Text)`
   text-transform: uppercase;
 `
 
+const StyledBox = styled(Box)`
+  display: inline-block;
+`
+
 export default function PhotoBlock({ caption, photos }) {
+  const width = photos.length > 1 ? '50%' : '100%';
   return (
-    <Box gap='xsmall' margin={{ vertical: 'small' }}>
-      {photos.map(item => <Image fit='contain' src={item.photo} width='100%' />)}
-      {caption && <CapitalText>{caption}</CapitalText>}
-    </Box>
+    <StyledBox margin={{ vertical: 'small' }}>
+      {photos.map(item => (
+        <Image fit='contain' src={item.photo} width={width} />
+      ))}
+      {caption && (
+        <CapitalText>
+          <Markdown>
+              {caption}
+          </Markdown>
+        </CapitalText>
+      )}
+    </StyledBox>
   )
 }
 
