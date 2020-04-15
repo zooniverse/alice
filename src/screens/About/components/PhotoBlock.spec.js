fc80579b4f9abf0f05a8afc04fc061d623bdcefd
Will Granger <twgranger@gmail.com>
2020-04-15 15:42:36 -0500

    Update ContentsTable and PhotoBlock Tests

--- src/screens/About/components/PhotoBlock.spec.js
+++ src/screens/About/components/PhotoBlock.spec.js
@@ -1,10 +1,35 @@
 import { shallow } from 'enzyme'
 import React from 'react'
-import PhotoBlock from './PhotoBlock'
+import PhotoBlock, { CapitalText, StyledImage } from './PhotoBlock'
+
+let wrapper
 
 describe('Component > PhotoBlock', function () {
+  beforeEach(function() {
+    const photos = [{ alt: 'A photo' },{ alt: 'Another photo' }]
+    wrapper = shallow(<PhotoBlock caption='A caption' photos={photos} />);
+  })
+
   it('should render without crashing', function () {
-    const wrapper = shallow(<PhotoBlock />);
     expect(wrapper).toBeDefined()
+  })
+
+  it('should render the correct number of photos', function () {
+    const images = wrapper.find(StyledImage)
+    expect(images.length).toBe(2)
+  })
+
+  it('should render a caption', function () {
+    const text = wrapper.find(CapitalText)
+    expect(text.length).toBe(1)
+  })
+
+  describe('with one photo', function () {
+    it('should render the photo at 100% width', function () {
+      const photo = [{ alt: 'Another photo' }]
+      wrapper = shallow(<PhotoBlock caption='A caption' photos={photo} />);
+      const image = wrapper.find(StyledImage).first()
+      expect(image.props().width).toBe('100%')
+    })
   })
 })
