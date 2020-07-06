21122da13458f7ae66ce28158ba56a0e6d83c36e
Will Granger <twgranger@gmail.com>
2020-07-06 11:47:21 -0500

    Refactor SVG View

--- src/components/SubjectViewer/components/SVGView/SVGView.js
+++ src/components/SubjectViewer/components/SVGView/SVGView.js
@@ -4,6 +4,8 @@ import { bool, string, number } from 'prop-types'
 import AppContext from 'store'
 import AnnotationsPane from '../AnnotationsPane'
 
+let startingPos = { x: 0, y: 0 }
+
 const SVG = styled.svg`
   height: 100%;
   width: 100%;
@@ -13,46 +15,38 @@ const SVG = styled.svg`
   }
 `
 
-let startingPos = { x: 0, y: 0 }
-
 const SVGView = React.forwardRef(function ({ disabled, height, url, transform, width}, ref) {
   if (url.length === 0 || disabled || !ref) return null;
+
+  const store = React.useContext(AppContext)
+  const [isMoving, setMove] = React.useState(false)
 
   const boundingBox = ref.current && ref.current.getBoundingClientRect()
   const viewerWidth = (boundingBox && boundingBox.width) || 0
   const viewerHeight = (boundingBox && boundingBox.height) || 0
   const viewBox = `${-viewerWidth/2} ${-viewerHeight/2} ${viewerWidth || 0} ${viewerHeight || 0}`
 
-  const store = React.useContext(AppContext)
-  const [isMoving, setMove] = React.useState(false)
-
   const onMouseDown = e => {
-    if (disabled) return
     startingPos = { x: e.clientX, y: e.clientY }
     setMove(true)
   }
-  const onMouseUp = e => {
-    if (disabled) return
-    setMove(false)
-  }
   const onMouseMove = e => {
-    if (disabled) return
-    if (isMoving) {
-      const difference = {
-        x: (e.clientX - startingPos.x) / store.image.scale,
-        y: (e.clientY - startingPos.y) / store.image.scale
-      }
-      startingPos = { x: e.clientX, y: e.clientY }
-      store.image.setTranslate(difference)
+    if (!isMoving) return
+
+    const difference = {
+      x: (e.clientX - startingPos.x) / store.image.scale,
+      y: (e.clientY - startingPos.y) / store.image.scale
     }
+    startingPos = { x: e.clientX, y: e.clientY }
+    store.image.setTranslate(difference)
   }
 
   return (
     <SVG
       onMouseDown={onMouseDown}
-      onMouseLeave={onMouseUp}
+      onMouseLeave={() => setMove(false)}
       onMouseMove={onMouseMove}
-      onMouseUp={onMouseUp}
+      onMouseUp={() => setMove(false)}
       viewBox={viewBox}
     >
       <g transform={transform}>
