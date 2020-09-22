27e566781e9f5d1839a0d02675cda35faffd9905
Will Granger <twgranger@gmail.com>
2020-09-22 11:08:32 -0500

    Add content when deleting non-duplicate frame

--- src/components/Modals/DeletePageModal/content.js
+++ src/components/Modals/DeletePageModal/content.js
@@ -6,7 +6,10 @@ const content = {
   },
   withoutDuplicates: {
     title: 'Delete contents of this page?',
-    firstParagraph: 'The selected page contains transcribed text data. Are you sure you want to delete these transcriptions?',
+    firstParagraph: `
+      The selected page contains transcribed text data. Are you sure you want to delete these transcriptions?
+      Deleting a frame removes associated contents only; non-duplicate images cannot be removed.
+    `,
     secondParagraph: 'This action can be undone using the Undo button in the toolbar.'
   }
 }
