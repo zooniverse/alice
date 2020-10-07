98c1a60e45f21d9531e388026e02ebe8f27b363e
Sam Blickhan <samantha@zooniverse.org>
2020-10-07 15:39:19 -0500

    Update Acknowledgements.js

--- src/screens/About/Acknowledgements.js
+++ src/screens/About/Acknowledgements.js
@@ -1,8 +1,16 @@
 Header Needed
 Import Statements Needed
 
-Samantha Blickhan
-Nicole Ciemniak
-Will Granger
-Becky Rother
-Zach Wolfenbarger
+
+The following Zooniverse team members contributed to the creation of ALICE:
+Samantha Blickhan, Project director
+Nicole Ciemniak, Back-end developer
+Will Granger, Front-end developer
+Coleman Krawczyk, Data scientist
+Shaun Noordin, Front-end developer
+Becky Rother, Designer
+Zach Wolfenbarger, Back-end developer
+
+Support for ALICE was provided by a Digital Humanities Advancement Grant from the 
+National Endowment for the Humanities (‘Advancing Access to Transcribed Text in 
+Citizen Humanities‘, Grant number HAA-263825-19).
