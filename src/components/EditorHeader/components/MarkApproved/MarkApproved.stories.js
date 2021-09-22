3c5b48b25d36d576ab4dd804e166a97819bf69e3
Jim O'Donnell <jim@zooniverse.org>
2021-09-22 21:39:29 +0100

    Convert stories to CSF

--- src/components/EditorHeader/components/MarkApproved/MarkApproved.stories.js
+++ src/components/EditorHeader/components/MarkApproved/MarkApproved.stories.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { Grommet } from 'grommet';
-import MarkApproved from './MarkApprovedContainer'
-import { mergedTheme } from '../../../../theme'
+import MarkApproved from './MarkApprovedContainer';
+import { mergedTheme } from '../../../../theme';
 
-storiesOf('MarkApproved', module)
-  .add('Default', () => (
-    <Grommet theme={mergedTheme}>
-      <MarkApproved />
-    </Grommet>
-  ))
+export default {
+  title: 'MarkApproved',
+};
+
+export const Default = () => (
+  <Grommet theme={mergedTheme}>
+    <MarkApproved />
+  </Grommet>
+);
